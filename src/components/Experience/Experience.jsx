8d12b4e8c5d78c7a469f6742c2695c3d8b438796
Piyush Mishra <112481912+Kajumishra@users.noreply.github.com>
2026-02-08 09:59:35 +0530

    Fix eslint and react plugin versions

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -37,7 +37,7 @@ const Experience = () => {
             >
               {/* Role, Company Name, and Date */}
               <div className="flex flex-row items-center gap-0.5 mb-2">
-                <div className="bg-gray-300 border-5 border-[#8245ec] w-14 h-14 rounded-full flex justify-center items-center shrink-0 relative z-10 w-full max-w-xl">
+                <div className="bg-gray-300 border-5 border-[#8245ec] w-10 h-10 rounded-full flex justify-center items-center shrink-0 relative z-10 w-full max-w-xl">
                   <img
                     src={experience.img}
                     alt={experience.company}
