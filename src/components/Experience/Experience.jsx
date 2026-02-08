e619ae3f416047afa2da535e5e8bd546bb38bdda
Piyush Mishra <112481912+Kajumishra@users.noreply.github.com>
2026-02-08 11:05:26 +0530

    Fix experience logo size

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -37,12 +37,12 @@ const Experience = () => {
             >
               {/* Role, Company Name, and Date */}
               <div className="flex flex-row items-center gap-0.5 mb-2">
-                <div className="bg-gray-300 border-5 border-[#8245ec] w- 100 h- 100 rounded-full flex justify-center items-center shrink-0 relative z-10 w-full max-w-xl">
-                  <img
-                    src={experience.img}
-                    alt={experience.company}
-                    className="w-full h-full object-cover rounded-full"
-                  />
+                <div className="bg-gray-300 border-5 border-[#8245ec] w-24 h-24 rounded-full flex justify-center items-center shrink-0 relative z-10">
+                      <img
+                          src={experience.img}
+                           alt={experience.company}
+                           className="w-20 h-20 object-contain rounded-full"  // Fixed!
+                      />
                 </div>
                 <div className="flex flex-col">
                   <h3 className="text-xl sm:text-2xl font-semibold text-white">
